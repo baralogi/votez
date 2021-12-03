61bc2d40558f9b73f151f0b12319a773c2dc98e2
Sebastianus Sembara <sembara9090@gmail.com>
2021-12-03 10:37:23 +0700

    fix: voting detail card

--- resources/views/pages/candidate/index.blade.php
+++ resources/views/pages/candidate/index.blade.php
@@ -17,18 +17,12 @@
                             @php
                                 $logoImage = $voting->logo ? $voting->logo_link : $voting->default_logo_link;
                             @endphp
-                            <img src="{{ $logoImage }}" style="object-fit: cover; object-position: 50% 0%;" width="150"
-                                height="150" class="rounded-circle elevation-2 mb-2 mr-5" alt="Logo Image" id="previewImage">
-                            <div class="clearfix"></div>
+                            <img alt="image" src="{{ $logoImage }}" class="rounded-circle author-box-picture" width="150"
+                                style="object-fit: cover; object-position: 50% 0%;">
                         </div>
                         <div class="author-box-details">
                             <div class="author-box-name">
                                 <a href="#">{{ $voting->name }}</a>
-                                @if ($voting->voting_status == 'Aktif')
-                                    <span class="badge badge-pill badge-success">Aktif</span>
-                                @else
-                                    <span class="badge badge-pill badge-danger">Tidak Aktif</span>
-                                @endif
                             </div>
                             <div class="author-box-job">{{ $voting->start_at_format . ' - ' . $voting->end_at_format }}
                             </div>
@@ -36,6 +30,14 @@
                                 <p>{{ $voting->description }}</p>
                             </div>
                             <div class="mb-2 mt-3">
+                                @if ($voting->voting_status == 'Aktif')
+                                    <span class="badge badge-pill badge-success">Aktif</span>
+                                @else
+                                    <span class="badge badge-pill badge-danger">Tidak Aktif</span>
+                                @endif
+                            </div>
+                            <div class="w-100 d-sm-none"></div>
+                            <div class="float-right mt-sm-0 mt-3">
                                 <a href="{{ route('votings.index') }}" class="btn btn-sm btn-info">Kembali</a>
                             </div>
                         </div>
