1fdc1b1174838f478f2064ec5113c120ace16075
ssembara <sebastian.sembara@gocement.com>
2022-06-16 22:39:05 +0700

    database: create UserCommitteeSeeder

--- database/seeders/UserCommitteeSeeder.php
+++ database/seeders/UserCommitteeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserCommitteeSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class UserCommitteeSeeder extends Seeder
             'name' => 'Sebastianus Sembara',
             'organization_id' => 1,
             'email' => 'me@sembara.xyz',
-            'password' => 'password',
+            'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ])->syncRoles(['panitia']);
     }
